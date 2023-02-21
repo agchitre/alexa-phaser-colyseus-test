58c52777dde40821b7a14e4382a4c2359f498306
agchitre <106405125+agchitre@users.noreply.github.com>
2023-02-20 22:20:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { GameRoom } from './server/multiplayer/rooms/GameRoom.js';
+import { monitor } from "@colyseus/monitor";
 
 //CHANGE THIS TO YOUR WEB APP URL
 const WEB_APP_URL = "https://colyseus-chat.herokuapp.com/";
@@ -20,7 +21,7 @@ app.use(express.static(__dirname + '/client'));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
+app.use("/colyseus", monitor());
 
 //SET UP ALEXA
 const alexaServer = new AlexaServer(app, WEB_APP_URL);
